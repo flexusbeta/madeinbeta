2e2c2d55a2ebe744154c23d080e76127abf9d6cb
flexus <flexus.beta@gmail.com>
2017-09-19 14:56:54 +0000

    Création model Room

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/default_image.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
+  belongs_to :user
 end
